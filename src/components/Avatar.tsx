2177f29d19a634650bdad3d385325f0373fdb076
JG-Souza <joaogsouzafma@gmail.com>
2025-10-31 10:49:09 -0300

    Cria componente de avatar para foto usuário

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+type AvatarProps = {
+    src: string;
+    alt: string;
+}
+
+export function Avatar({ src, alt }: AvatarProps) {
+    return (
+        <div className="w-12 h-12 rounded-full overflow-hidden border-2 border-gray-200">
+            <img
+            src={src}
+            alt={alt}
+            className="w-full h-full object-cover"
+            />
+        </div>
+    );
+}
